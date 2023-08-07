4214d5912fe3ce51a565d64c9542d3c14864829a
Espi Marisa <contact@espi.me>
2023-08-07 02:42:19 -0500

    disable a few really annoying rules that hinder my life

--- index.cjs
+++ index.cjs
@@ -111,6 +111,12 @@ module.exports = {
         "plugin:import/typescript",
       ],
       rules: {
+        // This is just too much... sorry <3
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-vars": "off",
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+
         "@typescript-eslint/no-unused-vars": "error",
         "@typescript-eslint/explicit-member-accessibility": [
           "error",
