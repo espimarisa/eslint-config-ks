8e6fba9beeb6d4d28cf5c33385e4650330d8deca
Espi Marisa <contact@espi.me>
2024-03-20 22:34:20 -0500

    chore: turn off 2 breaking unicorn rules

--- src/rules/node.mjs
+++ src/rules/node.mjs
@@ -6,6 +6,9 @@ import eslintPluginNode from "eslint-plugin-n";
 /** @type {import("eslint").Linter.RulesRecord} */
 const nodeRules = {
   ...eslintPluginNode.configs["flat/recommended"].rules,
+
+  // Breakage
+  "n/no-missing-import": "off",
 };
 
 export default nodeRules;
--- src/rules/unicorn.mjs
+++ src/rules/unicorn.mjs
@@ -6,6 +6,10 @@ import eslintPluginUnicorn from "eslint-plugin-unicorn";
 /** @type {import("eslint").Linter.RulesRecord} */
 const unicornRules = {
   ...eslintPluginUnicorn.configs["flat/recommended"].rules,
+
+  // This is really annoying
+  "unicorn/filename-case": "off",
+  "unicorn/prevent-abbreviations": "off",
 };
 
 export default unicornRules;
